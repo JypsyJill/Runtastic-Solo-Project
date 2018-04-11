e6ab2b99cf15c568ebf6c98bcedde227c287cd07
JypsyJill <jillyn_oc_ca@yahoo.com>
2018-04-11 12:10:47 -0600

    second attempt to remove

--- nightwatch.props.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    resourcePath: "C:\\Users\\Randy\\Documents\\DevMtn\\testing-resources\\",
-    seleniumServer: "selenium-server-standalone-3.11.0.jar",
-    chromedriver: "chromedriver.exe"
- }
